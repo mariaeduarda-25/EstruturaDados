c19d20d7b9ac8bbafd3b00274ea640025b39f53b
mariaeduarda-25 <sabinomariaeduarda123@gmail.com>
2025-05-18 18:21:04 -0300

    Atividade3

---
+++ exercicio3fila.py
@@ -0,0 +1,16 @@
+from stack import Stack
+
+class Filas:
+   def __init__(self):
+       self.entrada = Stack()
+       self.saida = Stack()
+
+   def enqueue(self, item):
+       self.entrada.push(elemento)
+       print(f"{elemento} enfileirado com sucesso.")
+
+   def is_empty(self):
+       return self.entrada.is_empty() and self.saida.is_empty()
+
+
+
